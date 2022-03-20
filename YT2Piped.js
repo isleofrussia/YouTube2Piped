acf6cee92325e5542ca8bcd0b1770a4f01becd51
isleofrussia <42836008+isleofrussia@users.noreply.github.com>
2022-03-20 16:25:28 +0300

    Update YT2Piped.js

--- YT2Piped.js
+++ YT2Piped.js
@@ -1,13 +1,13 @@
 // ==UserScript==
 // @name         YT2Piped
-// @namespace    https://isleofrussia.github.io/
+// @namespace    https://isleofrussia.github.io/YT2Piped
 // @version      0.1
-// @description  Redirect YouTube to Piped instance
+// @description  Redirect YouTube links to Piped
 // @author       MZ
-// @grant       none
-// @run-at      document-start
-// @include     *://www.youtube.com/*
-// @include     *://*.youtube.com/*
-// @include     *://*.youtu.be/*
+// @grant        none
+// @run-at       document-start
+// @include      *://www.youtube.com/*
+// @include      *://*.youtube.com/*
+// @include      *://*.youtu.be/*
 // ==/UserScript==
 window.location.replace("https://piped.kavin.rocks" + window.location.pathname + window.location.search);
